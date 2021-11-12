9239524ae4dd696feeaa88b278a88d352bc135aa
Mihlos <miguel.mish@gmail.com>
2021-11-12 20:43:16 +0100

    initial commit

---
+++ fastapi_tdd_docker/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.params import Depends
+from fastapi_tdd_docker.config import get_settings, Settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
